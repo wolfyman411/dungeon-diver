eac0e4b96801923ae747d93e22cce4e75332f0cb
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-08-04 15:29:09 -0400

    login page finished

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,16 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-      <h1>Dungeon Diver </h1>
-    </>
+    <div className="login__body">
+      <div className="login__wrapper fade-in">
+        <div className="login__header">dungeon diver.</div>
+        <div className="login__subtitle">log in / sign up</div>
+        <div className="login__form--wrapper">
+          <input className="login__form--input" type="text" placeholder="username" />
+          <input className="login__form--input" type="password" placeholder="password" />
+          <button className="btn">play</button>
+        </div>
+      </div>
+    </div>
   );
 }
