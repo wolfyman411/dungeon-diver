d840ff934182fbb3f0cde25ada44f8d833e95b5b
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-08-05 14:54:58 -0400

    class buttons

--- app/character/ClassButton.tsx
+++ app/character/ClassButton.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function ClassButton({className = "", classIcon = "", classDesc = <div></div>}) {
+export default function ClassButton({className = "",classIcon = "",classDesc = <div></div>,updateSelection = () => {}}: {
+  className?: string; classIcon?: string; classDesc?: React.ReactNode; updateSelection?: (className: string) => void}) {
   return (
-    <div className='class__button--wrapper'>
+    <div className='class__button--wrapper' onClick={() => updateSelection(className)}>
         <div className="class__name">{className}</div>
         <div className="box">
             <Image width={50} height={50} src={`/classes/${classIcon}`} alt={className} />
--- app/character/page.tsx
+++ app/character/page.tsx
@@ -1,18 +1,67 @@
-import React from 'react'
+"use client"
+
+import React, { useRef, useState } from 'react'
 import ClassButton from './ClassButton'
 
 export default function page() {
+
+  const classWrapperRef = useRef<HTMLDivElement|null>(null);
+  
+  const [errorMessage,setErrorMessage] = useState("")
+  const [chosenClass,setChosenClass] = useState("")
+
+  function updateSelection(selectedClass="") {
+
+    console.log(selectedClass)
+
+    if (classWrapperRef.current === null) {
+      return
+    }
+
+    for (const item of Array.from(classWrapperRef.current.children)) {
+      item.classList.remove("selected")
+    }
+
+    let ref:HTMLElement|undefined
+
+    if (selectedClass === "Warrior") {
+      ref = classWrapperRef.current?.children[0] as HTMLElement | undefined
+    }
+    else if (selectedClass === "Mage") {
+      ref = classWrapperRef.current?.children[1] as HTMLElement | undefined
+    }
+    else if (selectedClass === "Ranger") {
+      ref = classWrapperRef.current?.children[2] as HTMLElement | undefined
+    }
+
+    if (ref) {
+      ref.classList.add("selected")
+    }
+
+    setChosenClass(selectedClass)
+  }
+
+  function beginGame() {
+    if (chosenClass === "") {
+      setErrorMessage("Please select a class.")
+      return
+    }
+
+    
+  }
+
   return (
     <div className='container'>
       <div className="page">
         <div className="character__wrapper">
             <div className="character__section">
                 <div className="character__header">Pick a class.</div>
-                <div className="class__wrapper">
-                    <ClassButton className={'Warrior'} classIcon={'warrior.svg'} classDesc={<>Use <span className='red'>muscle</span> to hit your enemies with your <span className='blue'>sword</span>.</>}/>
-                    <ClassButton className={'Mage'} classIcon={'mage.svg'} classDesc={<>Use <span className='red'>magic</span> to hit your enemies with your <span className='blue'>spells</span>.</>}/>
-                    <ClassButton className={'Ranger'} classIcon={'ranger.svg'} classDesc={<>Use <span className='red'>moxie</span> to hit your enemies with your <span className='blue'>slingshot</span>.</>}/>
+                <div className="class__wrapper" ref={classWrapperRef}>
+                    <ClassButton className={'Warrior'} classIcon={'warrior.svg'} classDesc={<>Use <span className='red'>muscle</span> to hit your enemies with your <span className='blue'>sword</span>.</>} updateSelection={updateSelection}/>
+                    <ClassButton className={'Mage'} classIcon={'mage.svg'} classDesc={<>Use <span className='red'>magic</span> to hit your enemies with your <span className='blue'>spells</span>.</>} updateSelection={updateSelection}/>
+                    <ClassButton className={'Ranger'} classIcon={'ranger.svg'} classDesc={<>Use <span className='red'>moxie</span> to hit your enemies with your <span className='blue'>slingshot</span>.</>} updateSelection={updateSelection}/>
                 </div>
+                <div style={{paddingTop:24}} className='red'>{errorMessage}</div>
             </div>
             <div className="character__section">
                 <div className="character__header">Enter the world.</div>
