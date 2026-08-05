0bea0afb036fb288e0d53daf75604968c43ce63c
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-08-05 14:07:57 -0400

    partial login functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,74 @@
 "use client";
-import { useEffect } from "react";
+import { Character } from "@/classes/character";
+import { auth, db } from "@/firebase/firebase";
+import { signInWithCredential, UserCredential } from "firebase/auth";
+import { collection, doc, getDoc, getDocs, query, setDoc, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+
   useEffect(() => {
   }, []);
+
+  async function checkUsers() {
+
+    setErrorMessage("")
+
+    if (username.trim().length <= 0 || password.trim().length <= 0) {
+      setErrorMessage("Please fill out both the username and password!")
+      return
+    }
+
+    try {
+      const usersRef = collection(db,"users")
+      const q = query(usersRef, where("username", "==", username))
+      const querySnapshot = await getDocs(q)
+
+      // Log In
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0]
+        const userData = userDoc.data()
+
+        try {
+          const userCredential = await signInWithCredential(auth,{username,password})
+        } catch (e) {
+          setErrorMessage("Login failed.")
+        }
+      }
+
+      // Sign Up
+      else {
+        try {
+          const userCredential:UserCredential = null
+          const user = userCredential.user
+          const characterRef = await setDoc(doc(db,"character"), {
+            character_id:user.uid,
+            class: "",
+            hp: "",
+            magic:0,
+            moxie:0,
+            muscle:0,
+            wins:0,
+            xp:0,
+          })
+
+          await setDoc(doc(db,"users",user.uid), {
+            username: username
+          })
+        }
+        catch (e) {
+          setErrorMessage("Sign up failed.")
+        }
+      }
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   return (
     <div className="container">
@@ -12,9 +76,10 @@ export default function Home() {
         <div className="login__header">dungeon diver.</div>
         <div className="login__subtitle">log in / sign up</div>
         <div className="login__form--wrapper">
-          <input className="login__form--input" type="text" placeholder="username" />
-          <input className="login__form--input" type="password" placeholder="password" />
-          <button className="btn">play</button>
+          <div className="login__error red">{errorMessage}</div>
+          <input className="login__form--input" type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)}/>
+          <input className="login__form--input" type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+          <button className="btn" onClick={checkUsers}>play</button>
         </div>
       </div>
     </div>
--- classes/character.ts
+++ classes/character.ts
@@ -7,8 +7,8 @@ export class Character {
     muscle: number;
     wins: number;
     xp: number;
-    world_completion: Map<string, number>;
-    constructor(className: string, hp: number, magic: number, moxie: number, muscle: number, wins: number, xp: number, world_completion: Map<string, number>) {
+    world_completion: Map<string, number> = new Map();
+    constructor(className: string, hp: number, magic: number, moxie: number, muscle: number, wins: number, xp: number) {
         this.class = className;
         this.hp = hp;
         this.magic = magic;
@@ -16,6 +16,5 @@ export class Character {
         this.muscle = muscle;
         this.wins = wins;
         this.xp = xp;
-        this.world_completion = world_completion;
     }
 }
